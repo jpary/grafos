2c3fbf57ef4a411a8585e8c28e1759d86f2a9587
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2017-09-07 19:49:45 -0300

    Add files via upload

---
+++ Graphs.cpp
@@ -0,0 +1,80 @@
+#include "Graphs.h"
+#include "dfs.cpp"
+
+Graph repr(){
+	Graph G;
+	FILE* in;
+	FILE* out;
+	in = fopen("dblp.txt", "r");
+	out = fopen("output.txt", "w");
+	if (!in) fprintf(out, "Could not open file.\n");
+	else{
+		int n, a;
+		fscanf(in, "%d", &n);
+		printf("Please choose one of the 3\nPossible graph representations:\n\n");
+		printf("Adjacency List - 0\nAdjacency Vector - 1\nAdjacency Matrix - 2(not recommended for +250k vertices)\n\n");
+		cin >> a;
+		int x = G.initEssentials(n, a);
+		if (x) printf("Couldn't create graph.");
+		else{
+		
+			// Lista Encadeada
+
+			if (a == LINK_LIST){
+				G.initAdjList();
+				printf("Finished initializing.\n");
+				int a, b;
+				while (fscanf(in, "%d %d", &a, &b) == 2){
+					G.addEdgeList(a, b);
+				}
+				printf("Finished assigning edges.\n");
+			}
+
+			// Vetor
+
+			if (a == VECTOR){
+				G.initAdjVector();
+				printf("Finished initializing.\n");
+				int a, b;
+				while (fscanf(in, "%d %d", &a, &b) == 2){
+					G.addEdgeVector(a, b);
+				}
+				printf("Finished assigning edges.\n");
+			}
+
+			// Vector<bool>
+
+			if (a == BOOL_MATRIX){
+				G.initAdjMatrixBool();
+				printf("Finished initializing.\n");
+				int a, b;
+				while (fscanf(in, "%d %d", &a, &b) == 2){
+					G.addEdgeMatrixBool(a, b);
+				}
+				printf("Finished assigning edges.\n");
+			}
+
+			// Dynamic_Bitset
+	/*
+			if (a == BITSETM){
+				G.initAdjMatrixSet();
+				printf("Finished initializing.\n");
+				int a, b;
+				while (fscanf(in, "%d %d", &a, &b) == 2){
+					G.addEdgeMatrixSet(a, b);
+				}
+				printf("Finished assigning edges.\n");
+			}*/
+		}
+	}
+	G.printObj(out);
+	printf("Finished printing.\n");
+	fclose(in);
+	fclose(out);
+	return G;
+}
+
+int main(){
+	Graph grafo = repr();
+	iterDFS(grafo, 1);
+}
