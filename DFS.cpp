df6f2222781ad1e8e58fa889e3bb9cd5678078ec
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2017-09-08 20:59:42 -0300

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,81 +1,64 @@
 #include <stack>
 
 void iterDFS(Graph* grafo, int raiz, int size){
-	stack<int>* pilha;
-	pilha = new stack<int>;
+	stack<int> pilha;
 	int index = grafo->componentes.size()-1;
-	printf("%d\n", index);
 
 	int level = 0;
 	int father = 0;
 
-	pilha->push(raiz);
+	pilha.push(raiz);
 	int x;
-	printf("Finished initializing.\n");
 
-	while (pilha->size() > 0){
-		x = pilha->top();
-		printf("%d\n", x);
+	while (pilha.size() > 0){
+		x = pilha.top();
 		grafo->pai[x] = father;
 		grafo->nivel[x] = level;
 		grafo->descobertos[x-1] = 1;
 		grafo->componentes[index].push_back(x);
-		
+
 		if (grafo->representation == LINK_LIST){
-			LinkList* p = grafo->AdjList[x-1];
+			LinkList* p = new LinkList();
+			p = grafo->AdjList[x-1];
 			while (p){
-				printf("%d\n", p->vertex);
-				if (grafo->descobertos[(p->vertex)-1] == 0){
-					pilha->push(p->vertex);
-					p = p->nextVertex;
-					printf("%d\n", p->vertex);
-					printf("Pushing.\n");
-				}
+				if (grafo->descobertos[(p->vertex)-1] == 0)	pilha.push(p->vertex);
+				p = p->nextVertex;
 			}
 		}
 
 		if (grafo->representation == VECTOR){
 			for (int i = 0; i < grafo->AdjVector[x-1].size(); i++){
-				if (grafo->descobertos[i-1] == 0) pilha->push(grafo->AdjVector[x-1][i]);
-				printf("Pushing.\n");
+				if (grafo->descobertos[i-1] == 0) pilha.push(grafo->AdjVector[x-1][i]);
 			}
 		}
 
 		if (grafo->representation == BOOL_MATRIX){
 			for (int i = 0; i < grafo->vertexNum; i++){
-				if (grafo->AdjMatrixBool[x-1][i-1] == 1 && grafo->descobertos[i-1] == 0) pilha->push(i);
-				printf("Pushing.\n");
+				if (grafo->AdjMatrixBool[x-1][i-1] == 1 && grafo->descobertos[i-1] == 0) pilha.push(i);
 			}
 		}
 
 		father = x;
 		level++;
-		while (pilha->size() > 0 && grafo->descobertos[pilha->top()-1] == 1){
-			if (grafo->explorados[pilha->top()-1] == 0){
+		while (pilha.size() > 0 && grafo->descobertos[pilha.top()-1] == 1){
+			if (grafo->explorados[pilha.top()-1] == 0){
 				level--;
 				father = grafo->pai[father];
-				grafo->explorados[pilha->top()-1] = 1;
+				grafo->explorados[pilha.top()-1] = 1;
 				}
-			pilha->pop();
-			printf("Popping.\n");
+			pilha.pop();
 		}
 	}
 
 	int steps = grafo->componentes[index].size();
-	printf("%d\n", steps);
 
 	if (steps < size){
 		int i = 0;
-		while (grafo->descobertos[i-1] != 0){
-			i++;
-			printf("%d\n", i);
-		}
+		while (grafo->descobertos[i] & 1) i++;
 		vector<int> y;
 		grafo->componentes.push_back(y);
 		
-		printf("Next Phase.\n");
-
-		iterDFS(grafo, i, size-steps);
+		iterDFS(grafo, i+1, size-steps);
 	}
 }
 
@@ -88,11 +71,9 @@ void recDFS(Graph* grafo, int vertice, int father, int level){
 
 	if (grafo->representation == LINK_LIST){
 		LinkList* p = grafo->AdjList[vertice-1];
-		while (!p){
-			if (grafo->descobertos[(p->vertex)-1] == 0){
-				recDFS(grafo, p->vertex, vertice, level+1);
-				p = p->nextVertex;
-			}
+		while (p){
+			if (grafo->descobertos[(p->vertex)-1] == 0) recDFS(grafo, p->vertex, vertice, level+1);
+			p = p->nextVertex;
 		}
 	}
 
