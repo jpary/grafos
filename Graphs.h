467a191c894d2de9cc78b416c44b345c6195598f
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2017-09-10 12:29:07 -0300

    Update Graphs.h
    Graph class finished.
    And no more Dynamic Bitset.
    ...not today, JP.

--- Graphs.h
+++ Graphs.h
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <bitset>
 #include <stdio.h>
 #include <vector>
 #include <stdlib.h>
-#include <set>
-// #include <boost/dynamic_bitset.hpp>
 
 using namespace std;
 
 #define LINK_LIST 0
 #define VECTOR 1
 #define BOOL_MATRIX 2
-#define BITSET 3
 
 typedef struct a{
 	int vertex;
@@ -93,90 +89,63 @@ class Graph{
 			this->vertexDegree[v2-1]++;
 		}
 
-		void componentRemover(){
-			int index = this->componentes.size()-1;
-			printf("%d\n", this->componentes[index].size());
-			vector<int>::iterator inicio = this->componentes[index].begin();
-			vector<int>::iterator fim = this->componentes[index].end();
-			this->componentes[index-1].erase(inicio, fim);
-			this->componentes.pop_back();
-		}
-
 //-----------------Vetor de Adjacência-----------------------------------------------------------------//
 		
 		void initAdjVector(){
-			AdjVector = new vector<int> [vertexNum];
+			this->AdjVector = new vector<int> [vertexNum];
 		}
 
 		void addEdgeVector(int v1, int v2){
 			int size1 = AdjVector[v1-1].size();
 			int size2 = AdjVector[v2-1].size();
 
-			AdjVector[v1-1].push_back(v2);
-			AdjVector[v2-1].push_back(v1); 
+			this->AdjVector[v1-1].push_back(v2);
+			this->AdjVector[v2-1].push_back(v1); 
 
-			edgeNum++;
+			this->edgeNum++;
 
-			degrees[size1]--;
-			degrees[size1+1]++;
-			degrees[size2]--;
-			degrees[size2+1]++;
+			this->degrees[size1]--;
+			this->degrees[size1+1]++;
+			this->degrees[size2]--;
+			this->degrees[size2+1]++;
 		}
 
 //-------------------Matriz de Adjacência; vector<bool>------------------------------------------------//
 		
 		void initAdjMatrixBool(){
-			AdjMatrixBool = new vector<bool> [vertexNum];
+			this->AdjMatrixBool = new vector<bool> [vertexNum];
 			for (int i = 0; i < vertexNum; i++){
-				AdjMatrixBool[i].resize(vertexNum, false);
+				this->AdjMatrixBool[i].resize(vertexNum, false);
 			}
-			vertexDegree = new int [vertexNum] ();
+			this->vertexDegree = new int [vertexNum] ();
 		}
 
 		void addEdgeMatrixBool(int v1, int v2){
-			AdjMatrixBool[v1-1][v2-1] = 1;
-			AdjMatrixBool[v2-1][v1-1] = 1;
+			this->AdjMatrixBool[v1-1][v2-1] = 1;
+			this->AdjMatrixBool[v2-1][v1-1] = 1;
 
-			edgeNum++;
+			this->edgeNum++;
 			
-			degrees[vertexDegree[v1-1]]--;
-			degrees[vertexDegree[v1-1]+1]++;
-			degrees[vertexDegree[v2-1]]--;
-			degrees[vertexDegree[v2-1]+1]++;
+			this->degrees[vertexDegree[v1-1]]--;
+			this->degrees[vertexDegree[v1-1]+1]++;
+			this->degrees[vertexDegree[v2-1]]--;
+			this->degrees[vertexDegree[v2-1]+1]++;
 
-			vertexDegree[v1-1]++;
-			vertexDegree[v2-1]++;
+			this->vertexDegree[v1-1]++;
+			this->vertexDegree[v2-1]++;
 
 		}
+
+//-----------------Funções Adicionais-----------------------------------------------------------------//
 	
-//-------------------Matriz de Adjacência; boost::dynamic_bitset--------------------------------------//
-/*
-		void initAdjMatrixSet(){
-			boost:: dynamic_bitset<> x (vertexNum);
-			AdjMatrixSet = new boost::dynamic_bitset<> [vertexNum];
-			for (int i = 0; i < vertexNum; i++){
-				AdjMatrixSet[i] (x);
-			}
-			vertexDegree = new int [vertexNum] ();
+		void componentRemover(){
+			int index = this->componentes.size()-1;
+			vector<int>::iterator inicio = this->componentes[index].begin();
+			vector<int>::iterator fim = this->componentes[index].end();
+			this->componentes[index-1].erase(inicio, fim);
+			this->componentes.pop_back();
 		}
 
-		void addEdgeMatrixSet(int v1, int v2){
-			AdjMatrixSet[v1-1][v2-1] = 1;
-			AdjMatrixSet[v2-1][v1-1] = 1;
-			
-			edgeNum++;
-
-			degrees[vertexDegree[v1-1]]--;
-			degrees[vertexDegree[v1-1]+1]++;
-			degrees[vertexDegree[v2-1]]--;
-			degrees[vertexDegree[v2-1]+1]++;
-
-			vertexDegree[v1-1]++;
-			vertexDegree[v2-1]++;
-		}
-*/
-//--------------Printando informações do Grafo---------------------------------------------------------//
-	
 		void printObj(FILE* file){
 			fprintf(file, "# n = %d\n", vertexNum); // Printar número de vértices
 			fprintf(file, "# m = %d\n", edgeNum); // Printar número de arestas
