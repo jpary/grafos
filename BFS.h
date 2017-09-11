7f0be26d6730d8aa57fa87451a03b1e89711f2e1
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2017-09-10 21:08:03 -0300

    Update BFS.h

--- BFS.h
+++ BFS.h
@@ -1,9 +1,13 @@
 #include <queue>
 
-void BFS(Graph* grafo, int* raiz, int* size, FILE* file, int* number){
+int BFS(Graph* grafo, int* raiz, int* size, FILE* file, int* number, int d, int diam){
+
+	if (d == 0) grafo->descobertos.assign(grafo->vertexNum, 0);
 
 	queue<int> fila;
 	vector<int> vetor;
+
+	int level = 0;
 
 	grafo->pai[*raiz] = 0;
 	grafo->nivel[*raiz] = 0;
@@ -62,35 +66,62 @@ void BFS(Graph* grafo, int* raiz, int* size, FILE* file, int* number){
 				}
 			}
 		}
+	
+		level = grafo->nivel[x];
+	
 	}
 
-	int steps = vetor.size();
-	heapInsert(&(grafo->componentes), vetor);
+	if (d == 1){
+		int steps = vetor.size();
+		heapInsert(&(grafo->componentes), vetor);
 
-	if (steps <= *size){
-		while (grafo->componentes.size() > 0 && grafo->componentes[0].size() > (*size)-steps){
-			
-			vector<int> biggest;
-			heapRemove(grafo, (&biggest));
-			printf("Swapped.\n");
-			
-			file = fopen("components.txt", "a");
-			fprintf(file, "Componente Conexo #%d; Qtd. de Vértices: %d\n[", *number, (int) biggest.size());
-			
-			(*number)++;
+		if (steps <= *size){
+			while (grafo->componentes.size() > 0 && grafo->componentes[0].size() > (*size)-steps){
+				
+				vector<int> biggest;
+				heapRemove(grafo, (&biggest));
+				printf("Swapped.\n");
+				
+				file = fopen("components.txt", "a");
+				fprintf(file, "Componente Conexo #%d; Qtd. de Vértices: %d\n[", *number, (int) biggest.size());
+				
+				(*number)++;
+				int i = 0;
+
+				for (; i < biggest.size()-1; i++) fprintf(file, "%d, ", biggest[i]);
+				fprintf(file, "%d]\n\n", biggest[i]);
+				
+				fclose(file);
+				printf("Cleared memory.\n");
+			}
+			if (steps == *size) (*number)--;
+			*size -= steps;
 			int i = 0;
-
-			for (; i < biggest.size()-1; i++) fprintf(file, "%d, ", biggest[i]);
-			fprintf(file, "%d]\n\n", biggest[i]);
-			
-			fclose(file);
-			printf("Cleared memory.\n");
+			while (grafo->descobertos[i] & 1) i++;
+			*raiz = i+1;
+			return 0;
 		}
-		if (steps == *size) (*number)--;
-		*size -= steps;
-		int i = 0;
-		while (grafo->descobertos[i] & 1) i++;
-		*raiz = i+1;
-		return;
 	}
+	if (d == 0){
+		if (diam == 0){	
+			file = fopen("components.txt", "a");
+			fprintf(file, "Componente Conexo #%d; Qtd. de Vértices: %d\n[", *number, (int) vetor.size());
+			
+			int i = 0;
+			for (; i < vetor.size()-1; i++) fprintf(file, "%d, ", vetor[i]);
+			fprintf(file, "%d]\n\n", vetor[i]);
+						
+			fclose(file);
+		}
+		return level;
+	}
+}
+
+void diameter(Graph* grafo, int size, FILE* file, int* number){
+	int level = 0;
+	for (int i = 1; i <= size; i++){
+		level = BFS(grafo, &i, &size, file, number, 0, 1);
+		if (grafo->diametro < level) grafo->diametro = level;
+	}
+	printf("%d\n", grafo->diametro);
 }
