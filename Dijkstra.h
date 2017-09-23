cfff4efd0ef12cc3e11f6456a00532800f53fdf4
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2017-09-23 12:05:39 -0300

    Add files via upload

---
+++ Dijkstra.h
@@ -0,0 +1,66 @@
+#define INF -1;
+
+void dijkstra(Graph* grafo, int raiz){
+	vector<int> dist = new vector<int> ();
+	vector<int> pos = new vector<int> ();
+	vector<int> heap = new vector<int> ();
+
+	for (int i = 0; i < grafo->vertexNum; i++){
+		dist[i] = INF;
+		heap.push_back(i+1);
+		pos.push_back(i);
+	}
+
+	dist[raiz-1] = 0.0;
+	swap(&heap, raiz-1, 0);
+	swap(&pos, raiz-1, 0);
+
+	if (!heap.empty()){
+		int x = heap[0];
+		heapRemove(&heap, dist);
+
+		if (grafo->representation == LINK_LIST){
+			LinkList* p = new LinkList();
+			p = grafo->AdjList[x-1];
+			while (p){
+				int son = p->vertex;
+				float weight = p->weight;
+
+				if (dist[son-1] == INF || dist[son-1] > dist[x-1] + weight){
+					grafo->pai[son] = x;
+					grafo->nivel[son] = grafo->nivel[x]+1;
+					dist[son-1] = dist[x-1] + weight;
+					heapifyBottomUp(&heap, &distance, pos[son-1])
+				}
+				p = p->nextVertex;
+			}
+		}
+
+		if (grafo->representation == VECTOR){
+			for (int i = grafo->AdjVector[x-1].size()-1; i >= 0; i--){
+				
+				int son = grafo->AdjVector[x-1][i].vertex;
+				int weight = grafo->AdjVector[x-1][i].weight;
+				
+				if (dist[son-1] == INF || dist[son-1] > dist[x-1] + weight){
+					grafo->pai[son] = x;
+					grafo->nivel[son] = grafo->nivel[x]+1;
+					dist[son-1] = dist[x-1] + weight;
+					heapifyBottomUp(&heap, &distance, pos[son-1])
+				}
+			}
+		}
+
+		if (grafo->representation == MATRIX){
+			for (int i = 0; i < grafo->vertexNum; i++){
+				
+				if (grafo->AdjMatrixBool[x-1][i] == 1 && (dist[i] == INF || dist[i] > dist[x-1] + weight)){
+					grafo->pai[i+1] = x;
+					grafo->nivel[i+1] = grafo->nivel[x]+1;
+					dist[i] = dist[x-1] + weight;
+					heapifyBottomUp(&heap, &distance, pos[i])
+				}
+			}
+		}
+	}
+}
---
+++ WeightedGraphs.cpp
@@ -0,0 +1,137 @@
+#include "Graphs.h"
+#include "HeapFunctions.h"
+#include "DFS.h"
+#include "BFS.h"
+#include <string>
+
+WeightGraph* repr(){
+	WeightGraph* G;
+	
+	FILE* in;
+	FILE* out;
+	
+	in = fopen("dblp.txt", "r");
+	out = fopen("output.txt", "w");
+	
+	if (!in) fprintf(out, "Could not open file.\n");
+	else{
+		int n, a;
+		fscanf(in, "%d", &n);
+		
+		printf("Please choose one of the 3\nPossible graph representations:\n\n");
+		printf("Adjacency List - 0\nAdjacency Vector - 1\nAdjacency Matrix - 2 (not recommended for +250k vertices)\n\n");
+		
+		cin >> a;
+		
+		G = new WeightGraph;
+		int x = G->initEssentials(n, a);
+		
+		if (x) printf("Couldn't create graph.");
+		else{
+		
+			// Lista Encadeada
+
+			if (a == LINK_LIST){
+				printf("List\n\n");
+				G->initAdjList();
+				printf("Finished initializing.\n");
+				int a, b;
+				float w;
+				while (fscanf(in, "%d %d %f", &a, &b, &w) == 3){
+					G->addEdgeList(a, b, w);
+				}
+				printf("Finished assigning edges.\n");
+			}
+
+			// Vetor
+
+			if (a == VECTOR){
+				printf("Vector\n\n");
+				G->initAdjVector();
+				printf("Finished initializing.\n");
+				int a, b;
+				float w;
+				while (fscanf(in, "%d %d %f", &a, &b, &w) == 3){
+					G->addEdgeVector(a, b, w);
+				}
+				printf("Finished assigning edges.\n");
+			}
+
+			// Vector<float>
+
+			if (a == MATRIX){
+				printf("Matrix\n\n");
+				G->initAdjMatrix();
+				printf("Finished initializing.\n");
+				int a, b;
+				float w;
+				while (fscanf(in, "%d %d %f", &a, &b, &w) == 3){
+					G->addEdgeMatrixBool(a, b, w);
+				}
+				printf("Finished assigning edges.\n");
+			}
+		}
+	}
+	G->printObj(out);
+	printf("Finished printing.\n");
+	fclose(in);
+	fclose(out);
+	return G;
+}
+
+int main(){
+	WeightGraph* grafo = repr();
+	FILE* arq1;
+	arq1 = fopen("vertices.txt", "w");
+	
+	int search;
+	printf("\nWhich search method would you like?\n\n");
+	printf("BFS (Breadth-First Search) - 0\nDFS (Depth-First Search) - 1\nDiameter (BFS-only) - 2\n\n");
+	cin >> search;
+
+	FILE* arq2;
+	arq2 = fopen("components.txt", "w");
+	fclose(arq2);
+
+	string s;
+	if (search == 1) s = "DFS";
+	else s = "BFS";
+
+	int* a = new int (1);
+		
+	int* tamanho = new int (grafo->vertexNum);
+	if (search != 2){
+		
+		int root;
+		printf("\nEnter the 1st tree's root (the valid interval is [%d, %d]): ", 1, grafo->vertexNum);
+		cin >> root;
+		printf("\n");
+
+		int d;
+		printf("\nWould you like to do a single %s - 0\nOr calculate all of its components? - 1\n\n", s.c_str());
+		cin >> d;
+		
+		if (d == 1){
+			
+			while (*tamanho > 0){
+				if (search == 0) BFS(grafo, &root, tamanho, arq2, a, d, 0);
+				if (search == 1) DFS(grafo, &root, tamanho, arq2, a, d);
+				printf("%d\n", *tamanho);
+			}
+			fprintf(arq1, "Número de Componentes Conexos: %d\n\n", *a);
+		}
+
+		if (d == 0){
+			if (search == 0) BFS(grafo, &root, tamanho, arq2, a, d, 0);
+			if (search == 1) DFS(grafo, &root, tamanho, arq2, a, d);
+		}	
+
+		for (int k = 1; k <= grafo->vertexNum; k++){
+			if (grafo->descobertos[k-1] == 1) fprintf(arq1, "# %d = Pai: %d; Nível: %d\n", k, grafo->pai[k], grafo->nivel[k]);
+		}
+		printf("Finished printing.\n");
+	}
+	else{
+		diameter(grafo, grafo->vertexNum, arq2, a);
+	}
+}
