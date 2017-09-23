6412b38586a2f8ed2d5b4b7e0b9882c3a4396ea8
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2017-09-23 12:53:15 -0300

    Update WeightedGraphs.cpp

--- WeightedGraphs.cpp
+++ WeightedGraphs.cpp
@@ -86,52 +86,28 @@ int main(){
 	
 	int search;
 	printf("\nWhich search method would you like?\n\n");
-	printf("BFS (Breadth-First Search) - 0\nDFS (Depth-First Search) - 1\nDiameter (BFS-only) - 2\n\n");
+	printf("Dijkstra - 0\nPrim - 1\nKruskal - 2\n\n");
 	cin >> search;
 
 	FILE* arq2;
 	arq2 = fopen("components.txt", "w");
 	fclose(arq2);
 
-	string s;
-	if (search == 1) s = "DFS";
-	else s = "BFS";
-
 	int* a = new int (1);
 		
 	int* tamanho = new int (grafo->vertexNum);
-	if (search != 2){
 		
 		int root;
 		printf("\nEnter the 1st tree's root (the valid interval is [%d, %d]): ", 1, grafo->vertexNum);
 		cin >> root;
 		printf("\n");
 
-		int d;
-		printf("\nWould you like to do a single %s - 0\nOr calculate all of its components? - 1\n\n", s.c_str());
-		cin >> d;
-		
-		if (d == 1){
-			
-			while (*tamanho > 0){
-				if (search == 0) BFS(grafo, &root, tamanho, arq2, a, d, 0);
-				if (search == 1) DFS(grafo, &root, tamanho, arq2, a, d);
-				printf("%d\n", *tamanho);
-			}
-			fprintf(arq1, "Número de Componentes Conexos: %d\n\n", *a);
-		}
-
-		if (d == 0){
-			if (search == 0) BFS(grafo, &root, tamanho, arq2, a, d, 0);
-			if (search == 1) DFS(grafo, &root, tamanho, arq2, a, d);
-		}	
+		//Aqui entra Dijkstra/Prim
+		//Usar int search.
 
 		for (int k = 1; k <= grafo->vertexNum; k++){
 			if (grafo->descobertos[k-1] == 1) fprintf(arq1, "# %d = Pai: %d; Nível: %d\n", k, grafo->pai[k], grafo->nivel[k]);
 		}
 		printf("Finished printing.\n");
-	}
-	else{
-		diameter(grafo, grafo->vertexNum, arq2, a);
 	}
 }
